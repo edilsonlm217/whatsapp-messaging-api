fda70dec7d7572c4f5b925d610292f4f6f85869b
Seu Nome <edilsonlm217@hotmail.com>
2025-03-22 17:57:34 -0400

    feat: added Database and WhatsApp modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import { WhatsAppModule } from './core/whatsapp/whatsapp.module';
 
 @Module({
-  imports: [WhatsAppModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
+    WhatsAppModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
