f93f61229dcd24fcb274ba2b59e42f645f7b9625
Seu Nome <edilsonlm217@hotmail.com>
2025-03-22 21:06:30 -0400

    refactor: now endpoint return sessionEvents$ as observable directly

--- src/core/whatsapp/whatsapp.controller.ts
+++ src/core/whatsapp/whatsapp.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Param, Sse } from '@nestjs/common';
-import { Observable } from 'rxjs';
 import { WhatsAppService } from './whatsapp.service';
 
 @Controller('whatsapp')
@@ -9,7 +8,7 @@ export class WhatsAppController {
   @Sse('events/:sessionId')
   async startSessionAndListenEvents(
     @Param('sessionId') sessionId: string
-  ): Promise<Observable<any>> {
+  ) {
     try {
       const sessionEvents$ = await this.whatsappService.startSession(sessionId);
 
@@ -17,27 +16,7 @@ export class WhatsAppController {
         throw new Error(`Sessão ${sessionId} não encontrada.`);
       }
 
-      return new Observable(observer => {
-        sessionEvents$.subscribe({
-          next: event => {
-            observer.next({ type: 'message', data: event });
-
-            // Se a sessão foi iniciada com sucesso, finaliza o SSE após um tempo
-            if (event.type === 'sessão iniciada com sucesso') {
-              setTimeout(() => {
-                observer.complete();
-                console.log(`Conexão SSE para a sessão ${sessionId} encerrada.`);
-              }, 1000);
-            }
-          },
-          error: err => {
-            observer.error(err);
-          },
-          complete: () => {
-            observer.complete();
-          },
-        });
-      });
+      return sessionEvents$; // Retorna diretamente o Observable
     } catch (error) {
       throw new Error(`Erro ao iniciar a sessão ${sessionId}: ${error.message}`);
     }
