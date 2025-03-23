f42398c23fd40f65f1155e2c33e7f5542b20f330
Seu Nome <edilsonlm217@hotmail.com>
2025-03-22 21:18:04 -0400

    refactor: refactored session creation to SessionManager

--- src/core/whatsapp/whatsapp.module.ts
+++ src/core/whatsapp/whatsapp.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthStateModule } from './auth-state/auth-state.module';
 import { WhatsAppController } from './whatsapp.controller';
 import { WhatsAppService } from './whatsapp.service';
+import { SessionManager } from './session.manager.service';
 
 @Module({
   imports: [AuthStateModule],
   controllers: [WhatsAppController],
-  providers: [WhatsAppService],
+  providers: [WhatsAppService, SessionManager],
   exports: [WhatsAppService]
 })
 export class WhatsAppModule { }
