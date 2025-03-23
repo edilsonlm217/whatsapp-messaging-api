b28945ae17ba7e07d2e2440d3819697215c9242a
Seu Nome <edilsonlm217@hotmail.com>
2025-03-22 21:17:16 -0400

    refactor: refactored session creation to SessionManager

---
+++ src/core/whatsapp/session.manager.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { WhatsAppSession } from './whatsapp-session';
+import { AuthStateService } from './auth-state/auth-state.service';
+
+@Injectable()
+export class SessionManager {
+  private sessions: Map<string, WhatsAppSession> = new Map();
+
+  constructor(private readonly authStateService: AuthStateService) { }
+
+  /**
+   * Cria e armazena uma nova sessão do WhatsApp.
+   */
+  createSession(sessionId: string): WhatsAppSession {
+    const session = new WhatsAppSession(sessionId, this.authStateService);
+    this.sessions.set(sessionId, session);
+    return session;
+  }
+
+  /**
+   * Obtém uma instância da sessão específica.
+   */
+  getSession(sessionId: string): WhatsAppSession | undefined {
+    return this.sessions.get(sessionId);
+  }
+
+  /**
+   * Verifica se a sessão já está ativa.
+   */
+  isSessionActive(sessionId: string): boolean {
+    return this.sessions.has(sessionId);
+  }
+
+  /**
+   * Encerra uma sessão específica.
+   */
+  async stopSession(sessionId: string) {
+    const session = this.sessions.get(sessionId);
+    if (session) {
+      await session.desconectar();
+      this.sessions.delete(sessionId);
+      console.log(`Sessão ${sessionId} removida.`);
+    } else {
+      console.log(`Sessão ${sessionId} não encontrada.`);
+    }
+  }
+}
--- src/core/whatsapp/whatsapp.service.ts
+++ src/core/whatsapp/whatsapp.service.ts
@@ -1,77 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import { AuthStateService } from './auth-state/auth-state.service';
-import { WhatsAppSession } from './whatsapp-session';
+import { SessionManager } from './session.manager.service';
 import { Subject } from 'rxjs';
 
 @Injectable()
 export class WhatsAppService {
-  private sessions: Map<string, WhatsAppSession> = new Map();
   private globalEvents = new Subject<{ sessionId: string; type: string; data?: any }>();
 
-  constructor(private readonly authStateService: AuthStateService) { }
+  constructor(
+    private readonly sessionManager: SessionManager,
+  ) { }
 
   /**
    * Obtém ou cria uma nova sessão do WhatsApp.
    */
   async startSession(sessionId: string) {
-    if (this.isSessionActive(sessionId)) {
+    if (this.sessionManager.isSessionActive(sessionId)) {
       console.log(`Sessão ${sessionId} já está em execução.`);
-      return this.sessions.get(sessionId)!.sessionEvents$; // Retorna o Observable diretamente
+      return;
     }
 
     console.log(`Iniciando nova sessão ${sessionId}...`);
-    const session = this.createSession(sessionId);
-
-    await session.iniciarSessao();
-
-    return session.sessionEvents$; // Retorna o Observable dos eventos da sessão
-  }
-
-  /**
- * Verifica se a sessão já está ativa.
- */
-  private isSessionActive(sessionId: string): boolean {
-    return this.sessions.has(sessionId);
-  }
-
-  /**
-   * Cria e armazena uma nova sessão do WhatsApp.
-   */
-  private createSession(sessionId: string): WhatsAppSession {
-    const session = new WhatsAppSession(sessionId, this.authStateService);
-    this.sessions.set(sessionId, session);
-
-    this.subscribeToSessionEvents(sessionId, session);
-
-    return session;
-  }
-
-  /**
-   * Assina eventos da sessão e gerencia o fluxo global de eventos.
-   */
-  private subscribeToSessionEvents(sessionId: string, session: WhatsAppSession) {
+    const session = this.sessionManager.createSession(sessionId);
+    
     session.sessionEvents$.subscribe(({ type, data }) => {
       if (type === 'logged_out') {
-        this.sessions.delete(sessionId);
+        this.sessionManager.stopSession(sessionId);
         console.log(`Sessão ${sessionId} removida após logout.`);
       }
 
       this.globalEvents.next({ sessionId, type, data });
     });
-  }
 
-  /**
-   * Encerra uma sessão específica.
-   */
-  async stopSession(sessionId: string) {
-    const session = this.sessions.get(sessionId);
-    if (session) {
-      await session.desconectar();
-      this.sessions.delete(sessionId);
-      console.log(`Sessão ${sessionId} removida.`);
-    } else {
-      console.log(`Sessão ${sessionId} não encontrada.`);
-    }
+    await session.iniciarSessao();
+
+    return session.sessionEvents$;
   }
 
   /**
@@ -79,12 +41,5 @@ export class WhatsAppService {
    */
   get events$() {
     return this.globalEvents.asObservable();
-  }
-
-  /**
-   * Obtém uma instância da sessão específica.
-   */
-  getSession(sessionId: string): WhatsAppSession | undefined {
-    return this.sessions.get(sessionId);
   }
 }
