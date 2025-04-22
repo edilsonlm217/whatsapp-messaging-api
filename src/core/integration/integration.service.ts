ff58cb34f32f4bb0a34b62f54f4e879aba7eac0e
Seu Nome <edilsonlm217@hotmail.com>
2025-04-22 15:58:10 -0400

    feat: defined event-handler for socket.qrcode.generated

--- src/core/integration/integration.service.ts
+++ src/core/integration/integration.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { WhatsAppService } from '../whatsapp/whatsapp.service';
+import { SessionService } from '../session/session.service';
 
 @Injectable()
 export class IntegrationService {
-  constructor(private readonly whatsAppService: WhatsAppService) { }
+  constructor(
+    private readonly whatsAppService: WhatsAppService,
+    private readonly sessionService: SessionService
+  ) { }
 
   // Esse método será chamado quando o evento 'session.created' for emitido
   @OnEvent('session.created')
   async handleSessionCreated(payload: { sessionId: string }) {
     try {
-      console.log('Evento recebido no IntegrationService:', payload);
+      console.log('Evento recebido no IntegrationService (Session Created):', payload);
       await this.whatsAppService.create(payload.sessionId);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Esse método será chamado quando o evento 'socket.qrcode.generated' for emitido
+  @OnEvent('socket.qrcode.generated')
+  async handleQRCodeGenerated(payload: { sessionId: string; qrCode: string }) {
+    try {
+      console.log('Evento recebido no IntegrationService (QRCode Generated):', payload);
+      await this.sessionService.registerQRCode(payload.sessionId, payload.qrCode);
     } catch (error) {
       console.error(error);
     }
