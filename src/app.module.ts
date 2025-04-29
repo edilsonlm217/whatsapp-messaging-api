1bd2cd8c59c533b8191207c97d4e68756890724c
Seu Nome <edilsonlm217@hotmail.com>
2025-04-29 16:39:40 -0400

    feat: defined app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { EventStoreModule } from './database/event-store/event-store.module';
+import { MongoModule } from './database/mongo/mongo.module';
+import { SessionModule } from './core/session/session.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     EventEmitterModule.forRoot(),
+    EventStoreModule,
+    MongoModule,
+    SessionModule,
   ],
   controllers: [],
   providers: [],
