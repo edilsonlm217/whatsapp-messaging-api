e2bf5476068b361661a7232bc2b01b83738aa670
Seu Nome <edilsonlm217@hotmail.com>
2025-05-12 15:02:24 -0400

    feat: implemented event emition to ne baileys event

--- src/core/session/baileys-socket/baileys/baileys.service.ts
+++ src/core/session/baileys-socket/baileys/baileys.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { AuthenticationCreds, AuthenticationState, ConnectionState, makeWASocket, WASocket } from '@whiskeysockets/baileys';
+import { AuthenticationCreds, AuthenticationState, ConnectionState, makeWASocket, WASocket, WAMessageUpdate } from '@whiskeysockets/baileys';
 import { EventEmitterService } from 'src/infrastructure/structured-event-emitter/event.emitter.service';
 
 @Injectable()
@@ -9,6 +9,7 @@ export class BaileysService {
   createSocket(sessionId: string, state: AuthenticationState): WASocket {
     const socket = makeWASocket({ auth: state, printQRInTerminal: true });
 
+    // Evento de conexão
     socket.ev.on('connection.update', (update) => {
       this.eventEmitterService.emitEvent<Partial<ConnectionState>>(
         sessionId,
@@ -19,6 +20,7 @@ export class BaileysService {
       );
     });
 
+    // Evento de atualização das credenciais
     socket.ev.on('creds.update', () => {
       this.eventEmitterService.emitEvent<AuthenticationCreds>(
         sessionId,
@@ -27,6 +29,28 @@ export class BaileysService {
         BaileysService.name,
         socket.authState.creds
       );
+    });
+
+    // Captura de atualizações de status das mensagens (entrega, leitura, erro)
+    socket.ev.on('messages.update', (messageUpdates: WAMessageUpdate[]) => {
+      for (const { update, key } of messageUpdates) {
+        if (!key.id || !key.remoteJid) { continue }
+
+        const PRIMARY_DEVICE_REGEX = /^\d+@s\.whatsapp\.net$/;
+        const isPrimaryDeviceJid = PRIMARY_DEVICE_REGEX.test(key.remoteJid);
+        if (!isPrimaryDeviceJid) { continue }
+
+        this.eventEmitterService.emitEvent<WAMessageUpdate>(
+          sessionId,
+          'MessageUpdate',
+          'baileys-socket',
+          BaileysService.name,
+          {
+            update,
+            key
+          }
+        );
+      }
     });
 
     return socket;
