3246b811b0d183ba4e0551181ee645bfb7831db5
Seu Nome <edilsonlm217@hotmail.com>
2025-05-17 16:47:23 -0400

    refactor: defined WA_PRINT_QR_IN_TERMINAL as env

--- src/core/session/baileys-socket/baileys/baileys.service.ts
+++ src/core/session/baileys-socket/baileys/baileys.service.ts
@@ -7,7 +7,8 @@ export class BaileysService {
   constructor(private readonly eventEmitterService: EventEmitterService) { }
 
   createSocket(sessionId: string, state: AuthenticationState): WASocket {
-    const socket = makeWASocket({ auth: state, printQRInTerminal: true });
+    const printQRInTerminal = process.env.WA_PRINT_QR_IN_TERMINAL === 'true';
+    const socket = makeWASocket({ auth: state, printQRInTerminal });
 
     // Evento de conexão
     socket.ev.on('connection.update', (update) => {
