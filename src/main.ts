29cacf8f24e2a44c773d565cc195da242741e7b2
Seu Nome <edilsonlm217@hotmail.com>
2025-05-18 15:07:13 -0400

    refactor: disable node timeout for sse connectinos

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Remove o timeout padrão do Node.js (0 = sem timeout)
+  app.getHttpServer().setTimeout(0);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
